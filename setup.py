b3556b4a33d2a0729e72b89f46e256f3339e00ff
mentalblood <neceporenkostepan@gmail.com>
2022-10-09 15:41:22 +0300

    more correct package files

--- setup.py
+++ setup.py
@@ -3,19 +3,11 @@ from setuptools import setup, find_packages
 
 if __name__ == '__main__':
 
-	try:
-		with open('README.md', encoding='utf-8') as f:
-			long_description = f.read()
-	except FileNotFoundError:
-		long_description = ''
-
 	setup(
+
 		name='bandasync',
 		version='1.3.2',
-		description='Fast, working tool for downloading music from bandcamp',
-		long_description=long_description,
-		long_description_content_type='text/markdown',
-		author='mentalblood',
+		python_requires='>=3.10',
 		install_requires=[
 			'bs4',
 			'loguru',
@@ -23,5 +15,28 @@ if __name__ == '__main__':
 			'mutagen',
 			'python_version >= "3.10"'
 		],
+		keywords=['downloader'],
+		url='https://github.com/MentalBlood/bandasync',
+
+		description='Fast, working tool for downloading music from bandcamp',
+		long_description=open('README.md').read(),
+		long_description_content_type='text/markdown',
+
+		classifiers=[
+			'Development Status :: 5 - Production/Stable',
+			'Intended Audience :: End Users/Desktop'
+			'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+			'Typing :: Typed',
+			'Operating System :: OS Independent',
+			'Programming Language :: Python :: 3.10',
+			'License :: OSI Approved :: BSD License'
+		],
+
+		author='mentalblood',
+		author_email='neceporenkostepan@gmail.com',
+		maintainer='mentalblood',
+		maintainer_email='neceporenkostepan@gmail.com',
+
 		packages=find_packages()
+
 	)
